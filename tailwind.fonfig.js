0ed5614a34f4eaabf47ec00db5f11fa925adaaef
sajedul-dev <sajedulislamraja@gmail.com>
2023-07-26 18:02:17 +0600

    tailwind css project uploaded

---
+++ tailwind.fonfig.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        maxWidth: {
+            container: '1170px',
+        },
+          colors: {
+          clifford: '#da373d',
+          navbg:"#000",
+          fontcolor:"#fff",
+        },
+        fontSize:{
+            common:"18px",
+        }
+      }
+    }
+  }
